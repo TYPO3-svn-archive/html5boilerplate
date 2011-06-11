47959c0adc3561620c3d7f77071954e89eb0ab6c
svanman <svanman@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-06-11 17:53:42 +0000

    New tag for 0.5 Trunk updated
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/html5boilerplate/trunk@48566 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-$extensionPath = t3lib_extMgm::extPath('html5boilerplate');
-
-$autoloading = array(
-	'tx_html5boilerplate' => $extensionPath . 'Classes/class.tx_html5boilerplate.php',
-);
-
-return $autoloading;
-?>
