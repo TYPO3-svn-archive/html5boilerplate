47c23aeafc0ba27a4f944e845a05526d11d205b5
svanman <svanman@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-03 11:01:25 +0000

    Update ext_tables.php with links to the new static configuration files
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/html5boilerplate/trunk@53654 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -6,10 +6,10 @@ if (!defined('TYPO3_MODE')) {
 /**
  * Include TypoScript "HTML5 Boilerplate (config)"
  * 
- * Full blown example config of a TYPO3 website with htmlboilerplate
+ * Full blown example config of a TYPO3 website with html5boilerplate
  * Includes RealUrl, Indexed Search, english and UTF-8 as standard,
  * html5-doctype, conditional classes for <html>-tag, multidomain setup,
- * javascript and css optimization, disabled prefix comments and templavoila
+ * javascript and css optimization and disabled prefix comments 
  * 
  */
 t3lib_extMgm::addStaticFile($_EXTKEY,'static/html5boilerplate_config/', 'HTML5 Boilerplate (config)');
@@ -18,9 +18,28 @@ t3lib_extMgm::addStaticFile($_EXTKEY,'static/html5boilerplate_config/', 'HTML5 B
  * Include TypoScript "HTML5 Boilerplate (includes)"
  * 
  * Adds html5boilerplate libraries / files to main PAGE object (called 'page')
- * Includes Modernizr (http://www.modernizr.com/), JQuery (http://jquery.com/),
- * DD_belatedPNG.fix (http://www.dillerdesign.com/experiment/DD_belatedPNG/)
+ * Includes Modernizr (http://www.modernizr.com/), JQuery (http://jquery.com/)
  * and html5boilerplate javascript and css file
  */
 t3lib_extMgm::addStaticFile($_EXTKEY,'static/html5boilerplate_includes/', 'HTML5 Boilerplate (includes)');
+
+/**
+ * Include TypoScript "Mobile HTML5 Boilerplate (config)"
+ * 
+ * Full blown example config of a TYPO3 website with html5boilerplate mobile version
+ * Includes RealUrl, english and UTF-8 as standard,
+ * html5-doctype, conditional classes for <html>-tag, multidomain setup,
+ * javascript and css optimization and disabled prefix comments
+ * 
+ */
+t3lib_extMgm::addStaticFile($_EXTKEY,'static/mobile_html5boilerplate_config/', 'Mobile HTML5 Boilerplate (config)');
+
+/**
+ * Include TypoScript "Mobile HTML5 Boilerplate (includes)"
+ * 
+ * Adds html5boilerplate libraries / files to main PAGE object (called 'page')
+ * Includes Modernizr (http://www.modernizr.com/), JQuery (http://jquery.com/)
+ * and html5boilerplate javascript and css file
+ */
+t3lib_extMgm::addStaticFile($_EXTKEY,'static/mobile_html5boilerplate_includes/', 'Mobile HTML5 Boilerplate (includes)');
 ?>
